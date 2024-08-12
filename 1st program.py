dd585eb237686f5d180a0629d60d824966a0febe
Vishal Thapa <61000758+appa12-tech@users.noreply.github.com>
2024-08-12 11:32:01 +0545

    Add files via upload

---
+++ 1st program.py
@@ -0,0 +1,10 @@
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print(sum)
+list_me=[1,2,4,4,3,4]
+print(list_me)
+#my_list={"key1":123,'key2':'value1','key3':{'123':[1,2,'Vishal Thapa']}}
+#print(my_list['key3']['123'][2].upper())
+ 
